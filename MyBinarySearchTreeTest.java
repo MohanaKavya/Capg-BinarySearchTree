eda147277ef2ef60aa10260d34893e4906f4030f
Mohana Kavya K <kkunisettymohana@student.nitw.ac.in>
2020-10-12 20:31:30 +0530

    Modified : Test Case Modified to Add all Values in BST

--- MyBinarySearchTreeTest.java
+++ MyBinarySearchTreeTest.java
@@ -6,12 +6,24 @@ import org.junit.Test;
 
 public class MyBinarySearchTreeTest {
 	@Test
-	public void createBinaryTreeAddIntegersTest() {
+	public void createBinaryTreeAddAllValuesTest() {
 		MyBinarySearchTree<Integer> myBinarySearchTree=new MyBinarySearchTree<Integer>();
 		myBinarySearchTree.add(56);
 		myBinarySearchTree.add(30);
 		myBinarySearchTree.add(70);
 		int size=myBinarySearchTree.getSize();
 		assertEquals(3, size);
+		myBinarySearchTree.add(22);
+		myBinarySearchTree.add(40);
+		myBinarySearchTree.add(60);
+		myBinarySearchTree.add(95);
+		myBinarySearchTree.add(11);
+		myBinarySearchTree.add(65);
+		myBinarySearchTree.add(3);
+		myBinarySearchTree.add(16);
+		myBinarySearchTree.add(63);
+		myBinarySearchTree.add(67);
+		int fullSize=myBinarySearchTree.getSize();
+		assertEquals(13, fullSize);
 	}
 }
